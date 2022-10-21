9fd078fb62eaac268fe933c8b6ddfe3c763f5752
Zyler <508282+zylerdj@users.noreply.github.com>
2022-10-21 20:47:16 +0200

    Save Github Webhook in Database

--- src/server/handlers/github_webhooks.ts
+++ src/server/handlers/github_webhooks.ts
@@ -1,12 +1,25 @@
 
 import { log } from '../../log'
 
+import * as models from '../../models'
+
 export async function create(req, h) {
 
-  log.info('api.github.webhooks.create', {
-    payload: req.payload,
-    query: req.query
-  })
+  const { payload } = req
+
+  log.info('api.github.webhook.create', { payload })
+
+  try {
+
+    await models.GithubWebhook.create({ payload })
+
+    log.info('github.webhook.created', { payload })
+
+  } catch(error) {
+
+    log.error('github.webhook.create.error', error)
+
+  }
 
   return { success: true }
 
