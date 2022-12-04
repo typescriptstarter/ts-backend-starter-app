0b45711aa5fd914415b337b7401b6a08751e3ee1
Zyler <508282+zylerdj@users.noreply.github.com>
2022-12-04 02:44:13 +0100

    Remove web-ui from git

--- web-ui/.next/static/chunks/webpack.js
+++ web-ui/.next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "aa5a0543484d4333"; }
+/******/ 		__webpack_require__.h = function() { return "d5fb7919544bdc96"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
